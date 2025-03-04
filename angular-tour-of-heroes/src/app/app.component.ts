02deb535dcf960f504618d8785fee4febb077580
minhnvhe2103 <angminh2103@gmail.com>
2025-03-04 10:51:33 +0700

    intro

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from "../components/intro/intro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
